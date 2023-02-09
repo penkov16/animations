e7697c54756241b5df0f297d6e92d99e62f1b8cf
Пеньков А.С <penkov-sasha@mail.ru>
2023-02-09 15:22:37 +0300

    Anim

---
+++ app/src/main/java/ru/netology/statsview/AppActivity.kt
@@ -0,0 +1,20 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.statsview.ui.StatsView
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(
+            500F,
+            500F,
+            500F,
+            500F,
+        )
+    }
+}
---
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -0,0 +1,131 @@
+package ru.netology.statsview.ui
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.content.withStyledAttributes
+import ru.netology.statsview.R
+import ru.netology.statsview.utils.AndroidUtils
+import kotlin.math.min
+import kotlin.random.Random
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet?,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : View(
+    context,
+    attributeSet,
+    defStyleAttr,
+    defStyleRes,
+) {
+    private var radius = 0F
+    private var center = PointF(0F, 0F)
+    private var oval = RectF(0F, 0F, 0F, 0F)
+
+    private var lineWidth = AndroidUtils.dp(context, 5F).toFloat()
+    private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
+    private var colors = emptyList<Int>()
+
+    private var progress = 0F
+    private var valueAnimator: ValueAnimator? = null
+
+    init {
+        context.withStyledAttributes(attributeSet, R.styleable.StatsView) {
+            lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
+            fontSize = getDimension(R.styleable.StatsView_fontSize, fontSize)
+            val resId = getResourceId(R.styleable.StatsView_colors, 0)
+            colors = resources.getIntArray(resId).toList()
+        }
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = lineWidth
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+    }
+
+    private val textPaint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        textSize = fontSize
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = fontSize
+    }
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            field = value
+            update()
+        }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWidth / 2
+        center = PointF(w / 2F, h / 2F)
+        oval = RectF(
+            center.x - radius,
+            center.y - radius,
+            center.x + radius,
+            center.y + radius,
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (data.isEmpty()) {
+            return
+        }
+
+        val paintColors = data.mapIndexed { index, _ ->
+            colors.getOrElse(index) { generatorRandomColor() }
+        }
+
+        val initialAngle = -90F
+        var startAngle = initialAngle
+        data.forEachIndexed { index, datum ->
+            val angle = datum / data.sum() * 360F
+            paint.color = paintColors[index]
+            canvas.drawArc(oval, startAngle + 360F * progress, angle * progress, false, paint)
+            startAngle += angle
+        }
+        paint.color = paintColors[0]
+        val minSweepAngle = 0.01F
+        canvas.drawArc(oval, initialAngle + 360F * progress, minSweepAngle, false, paint)
+
+        canvas.drawText(
+            "%.2f%%".format(100F),
+            center.x,
+            center.y + textPaint.textSize / 4,
+            textPaint,
+        )
+    }
+
+    private fun update() {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 2000
+            interpolator = LinearInterpolator()
+        }.also {
+            it.start()
+        }
+    }
+
+    private fun generatorRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+}
